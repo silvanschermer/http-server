df7630bebbd320270d62c04ff4c4e346eaa4a740
silvanschermer <dev@silvanchermer.com>
2025-10-18 17:45:36 +0200

    Update Main and Logger with docstrings. Add checkstyle plugin for code quality enforcement.

--- src/main/java/com/silvanschermer/Main.java
+++ src/main/java/com/silvanschermer/Main.java
@@ -2,7 +2,14 @@ package com.silvanschermer;
 
 import com.silvanschermer.http.HttpServer;
 
+/** The main application entry point. */
 public class Main {
+
+  /**
+   * Starts the HTTP server on a specified port.
+   *
+   * @param args Command-line arguments, which may include a custom port using --port=<port_number>.
+   */
   static void main(String[] args) {
     int port = 8080;
     for (String arg : args) {
@@ -13,6 +20,13 @@ public class Main {
     new HttpServer(port).start();
   }
 
+  /**
+   * Parses a command-line argument to extract the port number.
+   *
+   * @param arg The command-line argument, expected to start with --port= followed by the port
+   *     number.
+   * @return The parsed port number if the argument is valid; otherwise, null.
+   */
   private static Integer parsePort(String arg) {
     return arg.startsWith("--port=") ? Integer.parseInt(arg.substring("--port=".length())) : null;
   }
--- src/main/java/com/silvanschermer/utils/Logger.java
+++ src/main/java/com/silvanschermer/utils/Logger.java
@@ -1,11 +1,22 @@
 package com.silvanschermer.utils;
 
+/** A simple logger. */
 public class Logger {
 
+  /**
+   * Logs a message to stdout.
+   *
+   * @param message The message to be logged.
+   */
   public static void info(String message) {
     System.out.println(message);
   }
 
+  /**
+   * Logs a message to stdout.
+   *
+   * @param message The message to be logged.
+   */
   public static void error(String message) {
     System.err.println(message);
   }
